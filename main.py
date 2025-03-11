f0483c6c521b41a49447249cfba9e290d4ef9318
ilariacogerino <s312806@studenti.polito.it>
2025-03-11 11:12:31 +0100

    lab02

--- main.py
+++ main.py
@@ -7,19 +7,26 @@ while(True):
 
     t.printMenu()
 
-    t.loadDictionary("filename.txt")
+    t.loadDictionary("dictionary.txt")
 
     txtIn = input()
 
     # Add input control here!
 
     if int(txtIn) == 1:
-        print()
-        txtIn = input()
+        print("Ok, quale parola devo aggiungere?")
+        parola = input()
+        t.handleAdd(parola)
         pass
     if int(txtIn) == 2:
-        pass
+        print("Ok, quale parola devo cercare?")
+        parola = input()
+        t.handleTranslate(parola)
     if int(txtIn) == 3:
-        pass
+        print("Ok, quale parola devo cercare?")
+        parola = input()
+        t.handleWildCard()
     if int(txtIn) == 4:
+        t.loadDictionary()
+    if int(txtIn) == 5:
         break
--- translator.py
+++ translator.py
@@ -4,10 +4,15 @@ class Translator:
         pass
 
     def printMenu(self):
-        # 1. Aggiungi nuova parola
-        # 2. Cerca una traduzione
-        # 3. Cerca con wildcard
-        # 4. Exit
+        print("--------------------------------")
+        print("Translator Alien-Italian")
+        print("--------------------------------")
+        print("1. Aggiungi nuova parola")
+        print("2. Cerca una traduzione")
+        print("3. Cerca con wildcard")
+        print("4. Stampa tutto il dizionario")
+        print("5. Exit")
+        print("--------------------------------")
         pass
 
     def loadDictionary(self, dict):
@@ -16,12 +21,25 @@ class Translator:
 
     def handleAdd(self, entry):
         # entry is a tuple <parola_aliena> <traduzione1 traduzione2 ...>
+        aliena = entry.split(" ")[0]
+        italiana = entry.split(" ")[1]
+        print(f"['{aliena}' - '{italiana}']")
+        with open("dictionary.txt", "a", encoding="utf-8") as file:
+            file.write(aliena + "\t" + italiana + "\n")
+        print('Aggiunta!')
         pass
 
     def handleTranslate(self, query):
-        # query is a string <parola_aliena>
+        with open("dictionary.txt", "r", encoding="utf-8") as file:
+            for line in file:
+                if line.split(" ")[0].__contains__(query):
+                    traduzione = line.split(" ")[1]
+                    #traduzione.strip()
+                    print(f"[{traduzione}]")
         pass
 
     def handleWildCard(self,query):
         # query is a string with a ? --> <par?la_aliena>
         pass
+
+
